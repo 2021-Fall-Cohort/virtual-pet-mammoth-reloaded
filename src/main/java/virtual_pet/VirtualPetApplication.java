800b3f659ebbfbe5f4682e55672ad16916e70d21
Aman Berhanemeskel <73969018+RedSonAzriel@users.noreply.github.com>
2021-09-16 15:15:03 -0400

    Created initial name and color promote along with age portion of tick method.

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,44 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
-    public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
-    }
+    public static void main(String[] args){
 
+        //Mammoth Name from User
+        System.out.println("What is your pets name? ");
+        Scanner input = new Scanner(System.in);
+        String mammothName = input.nextLine();
+        System.out.println("Welcome to the wintry world " + mammothName + "!");
+
+        //Mammoth Pet's fur Color
+        System.out.println("What color is your pet Mammoth? Red, Green, Orange, Brown, Blue, Yellow, or Indigo?");
+        input = new Scanner(System.in);
+        String mammothFurColor = input.nextLine();
+        System.out.println("Your pet " + mammothName  + ", is " + mammothFurColor + "!");
+
+//        //Mammoth Age
+//        System.out.println("How old is your pet?");
+//        input = new Scanner(System.in);
+//        int mammothAge = input.nextLine();
+//        System.out.println ("Your pet " + mammothName + ", is " + mammothFurColor + ", and " + mammothAge + " years old! ");
+
+        Mammoth userMammoth = new Mammoth(mammothName, mammothFurColor, -1, 0, 10,  1);
+
+        for(int y = 0; y<=10; y++){
+            userMammoth.tick();
+            userMammoth.hung();
+        }
+
+        //Do you want to feed your pet? (Feed Function)
+//        System.out.println("");
+//        input = new Scanner(System.in);
+//        String mammothTuskLength = input.nextLine();
+//        System.out.println ("Do you want to feed your pet?");
+
+
+
+    }
 }
+
